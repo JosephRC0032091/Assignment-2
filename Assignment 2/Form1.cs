613b26c974b8b2392a1604ca1e32b0306787b4eb
Joseph Bamber <rc0032091@student.riversidecollege.ac.uk>
2019-01-07 15:45:54 +0000

    Completed

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -16,5 +16,73 @@ namespace Assignment_2
         {
             InitializeComponent();
         }
+
+        private void SeriesCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double UserInput = double.Parse(textBox1.Text);
+                double UserInput2 = double.Parse(textBox2.Text);
+                double UserInput3 = double.Parse(textBox3.Text);
+                double Answer = UserInput + UserInput2 + UserInput3;
+                label2.Text = Answer.ToString();
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Please Read Instuctions And Attempt Again, if further problems occur restart.");
+
+            }
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void ParallelCalculate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double Input = double.Parse(textBox4.Text);
+                double Input2 = double.Parse(textBox5.Text);
+                double Top = Input * Input2;
+                double Bottom = Input + Input2;
+                double Answer = Top / Bottom;
+                label7.Text = Answer.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Please Read Instuctions And Attempt Again, if further problems occur restart.");
+            }
+
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
